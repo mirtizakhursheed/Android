d36c615715dee9cf4bad3e8e63b3e82884a9fc40
mirtizakhursheed <m.irtiza.khursheed@gmail.com>
2025-09-08 11:06:05 +0400

    Addition : Constants added

--- Todo List App - Jetpack Compose/app/src/main/java/com/irtiza/todolist/navigation/Screens.kt
+++ Todo List App - Jetpack Compose/app/src/main/java/com/irtiza/todolist/navigation/Screens.kt
@@ -2,9 +2,21 @@ package com.irtiza.todolist.navigation
 
 import androidx.navigation.NavController
 import com.irtiza.todolist.enums.Action
+import com.irtiza.todolist.util.Constants.LIST_SCREEN
+import com.irtiza.todolist.util.Constants.TASK_SCREEN
 
 class Screens(navController: NavController) {
     val list : (Action) -> Unit = { action ->
-        navController.navigate("list/${action.name}")
+        navController.navigate("list/${action.name}", builder = {
+            popUpTo(LIST_SCREEN) { inclusive = true }
+        })
     }
+
+    val task : (Int) -> Unit = { taskId ->
+        navController.navigate("task/${taskId}") {
+            popUpTo(TASK_SCREEN) { inclusive = true }
+        }
+    }
+
+
 }
--- Todo List App - Jetpack Compose/app/src/main/java/com/irtiza/todolist/util/Contants.kt
+++ Todo List App - Jetpack Compose/app/src/main/java/com/irtiza/todolist/util/Contants.kt
@@ -6,4 +6,6 @@ object Constants {
     const val DATABASE_TABLE= "todo_table"
     const val DATABASE_NAME : String = "todo_database"
     const val ZERO = 0
+    const val LIST_SCREEN = "list/action"
+    const val TASK_SCREEN = "task/action"
 }
