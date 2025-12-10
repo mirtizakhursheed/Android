7d2da6815f372ab7b90598cfb214ee6a928c4a51
Muhammad Irtiza Khursheed <m.irtiza.khursheed@gmail.com>
2025-12-10 11:33:21 +0400

    Basic project created

--- Food Tracker App - Jetpack Compose/core/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-import org.gradle.kotlin.dsl.resolver.buildSrcSourceRootsFilePath
-
-plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
-}
-
-// Correct usage of apply for external scripts in Kotlin DSL
-//apply(from = "${buildSrcSourceRootsFilePath}/basemodule.gradle.kts")
