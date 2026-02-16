8037bcfa11fb3a765f7883bec2810e0459e4b200
Muhammad Irtiza Khurshid <m.irtiza.khursheed@gmail.com>
2026-02-16 14:57:15 +0400

    2- Multiple Projects - Dollar Counter - Base project is added

--- 2- Multiple Projects - Jetpack Compose/1- Portfolio/README.md
+++ 2- Multiple Projects - Jetpack Compose/1- Portfolio/README.md
@@ -1,4 +1,6 @@
 ## PORTFOLIO APP - ANDROID - JETPACK COMPOSE
 
 
-![App Screenshot](1.png)
+![App Screenshot 1](1.png)
+
+![App Screenshot 2] (2.png)
---
+++ 2- Multiple Projects - Jetpack Compose/2- Dollar Counter/app/src/main/java/com/irtiza/a2_dollarcounterapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.irtiza.a2_dollarcounterapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.irtiza.a2_dollarcounterapp.ui.theme._2DollarCounterAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            _2DollarCounterAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    _2DollarCounterAppTheme {
+        Greeting("Android")
+    }
+}
