83da17b76a1c7fd1e0ad288c0b6ec0d30d4529d9
Muhammad Irtiza Khurshid <m.irtiza.khursheed@gmail.com>
2026-02-16 15:35:16 +0400

    2- Multiple Projects - Dollar Counter - App is completed

--- 2- Multiple Projects - Jetpack Compose/2- Dollar Counter/app/src/main/java/com/irtiza/a2_dollarcounterapp/MainActivity.kt
+++ 2- Multiple Projects - Jetpack Compose/2- Dollar Counter/app/src/main/java/com/irtiza/a2_dollarcounterapp/MainActivity.kt
@@ -4,44 +4,90 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableIntState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.irtiza.a2_dollarcounterapp.ui.theme._2DollarCounterAppTheme
+import androidx.compose.ui.unit.dp
+import com.irtiza.a2_dollarcounterapp.ui.theme.DollarCounterAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            _2DollarCounterAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            DollarCounterAppTheme {
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp() {
+    Surface(modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background) {
+        DollarCounter()
+    }
 }
+
+@Composable
+fun DollarCounter() {
+    val counter = remember { mutableIntStateOf(1) }
+
+    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Text(text = "$${counter.value *100}", style = MaterialTheme.typography.displayLarge)
+        Spacer(Modifier.height(180.dp))
+
+        ComposeButton {
+            counter.intValue++
+        }
+    }
+}
+
+@Composable
+fun ComposeButton(onClick :() -> Unit) {
+    Card(modifier = Modifier.clip(CircleShape)
+        .size(120.dp).clickable {
+            onClick.invoke()
+        }, shape = CircleShape, colors = CardDefaults.cardColors(
+        containerColor = Color.Yellow
+    )  ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Tap", style = MaterialTheme.typography.headlineLarge)
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    _2DollarCounterAppTheme {
-        Greeting("Android")
+    DollarCounterAppTheme {
+        MyApp()
     }
 }
--- 2- Multiple Projects - Jetpack Compose/2- Dollar Counter/app/src/main/java/com/irtiza/a2_dollarcounterapp/ui/theme/Theme.kt
+++ 2- Multiple Projects - Jetpack Compose/2- Dollar Counter/app/src/main/java/com/irtiza/a2_dollarcounterapp/ui/theme/Theme.kt
@@ -34,7 +34,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun _2DollarCounterAppTheme(
+fun DollarCounterAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
